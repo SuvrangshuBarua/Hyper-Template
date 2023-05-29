195e6ccc261e189f03ec7460e8e61ddd617887b6
Suvrangshu Barua <suvrangshu.turno@gmail.com>
2023-05-29 21:47:13 +0600

    some approach have changed for SVBookmark

--- Assets/Grimhawk/ToolBag/Editor/SceneBookmark/SVBookmark.cs
+++ Assets/Grimhawk/ToolBag/Editor/SceneBookmark/SVBookmark.cs
@@ -1,38 +1,81 @@
 using UnityEngine;
 using UnityEditor;
 using System;
-
-[Serializable]
-struct SceneViewBookmark
+/// <summary>
+/// Bookmark class to store actual bookmark data
+/// </summary>
+[System.Serializable]
+public class SVBookmark
 {
-    public Vector3 pivot;
-    public Quaternion rotation;
-    public float size;
-    public bool in2DMode;
+    #region Private Variables
+    [SerializeField]
+    private string name;
+    [SerializeField]
+    private Vector3 position;
+    [SerializeField]
+    private Vector3 rotation;
+    [SerializeField]
+    private bool isOrtho;
+    [SerializeField]
+    private bool is2D;
+    [SerializeField]
+    private float size;
+    [SerializeField]
+    private Texture2D thumbnail;
+    #endregion
 
-    public int svTextureSizeX;
-    public int svTextureSizeY;
+    #region Properties
+    public string Name => name;
+    public Vector3 Position => position;    
+    public Vector3 Rotation => rotation;    
+    public bool IsOrthographic => isOrtho;
+    public bool Is2D => is2D;
+    public float Size => size;
+    public bool IsSet => !string.IsNullOrEmpty(Name);
+    public Texture2D Thumbnail { get { return thumbnail; } internal set { thumbnail = value; } }
+    #endregion
 
-    public string svTextureBase64String;
-
-    public SceneViewBookmark(SceneView sceneView, Texture2D sceneViewTexture)
+    /// <summary>
+    /// Constructor to set fields of an instance
+    /// </summary>
+    /// <param name="name">Name of Bookmark</param>
+    /// <param name="position">World Space position of camera</param>
+    /// <param name="rotation">Rotaion in eular angles of camera</param>
+    /// <param name="size">Size of camera</param>
+    /// <param name="isOrthographic">Bookmark is orthographic</param>
+    /// <param name="is2D">Bookmark using 2D mode</param>
+    internal SVBookmark(string name, Vector3 position, Vector3 rotation, float size, bool isOrthographic,  bool is2D)
     {
-        this.pivot = sceneView.pivot;
-        this.rotation = sceneView.rotation;
-        this.size = sceneView.size;
-        this.in2DMode = sceneView.in2DMode;
-
-        svTextureSizeX = sceneViewTexture.width;
-        svTextureSizeY= sceneViewTexture.height;
-        svTextureBase64String = System.Convert.ToBase64String(sceneViewTexture.EncodeToPNG());
+        this.name = name;
+        this.position = position;
+        this.rotation = rotation;
+        this.size = size;
+        this.isOrtho = isOrthographic;
+        this.is2D = is2D;
     }
-    public Texture2D GetSVTexture()
+    /// <summary>
+    /// Sets value for a given scene view as the value of bookmark
+    /// </summary>
+    /// <param name="sceneView">Target scene view</param>
+    public void SetSceneViewAsBookmark(SceneView sceneView)
     {
-        if (string.IsNullOrEmpty(svTextureBase64String)) return null;
-        byte[] textureBytes = System.Convert.FromBase64String(svTextureBase64String);
-        var texture = new Texture2D(svTextureSizeX, svTextureSizeY);
-        texture.LoadImage(textureBytes);
-
-        return texture; 
+        sceneView.pivot = position;
+        sceneView.in2DMode = is2D;
+        if(!sceneView.in2DMode)
+        {
+            sceneView.rotation = Quaternion.Euler(rotation);
+        }
+        sceneView.orthographic = isOrtho;
+        sceneView.size = size;
+    }
+    /// <summary>
+    /// Create a new bookmark object with a given name from the current camera view of a given scene view
+    /// </summary>
+    /// <param name="name">Name of the bookmark</param>
+    /// <param name="sceneView">SceneView from which to generate the bookmark</param>
+    /// <returns></returns>
+    public static SVBookmark CreateFromSceneView(string name, SceneView sceneView)
+    {
+        return new SVBookmark(name, sceneView.pivot, sceneView.rotation.eulerAngles, sceneView.size, sceneView.orthographic, sceneView.in2DMode);
     }
 }
--- Assets/Grimhawk/ToolBag/Editor/SceneBookmark/SVBookmarkManager.cs
+++ Assets/Grimhawk/ToolBag/Editor/SceneBookmark/SVBookmarkManager.cs
@@ -3,8 +3,24 @@ using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
+
+/// <summary>
+/// Has a few responsibilities 
+/// 1. Publishes events reporting bookmark opened, saved and removed 
+/// 2. A getter for Scene View Bookmark Icon
+/// 3. A getter for Bookmark Icon
+/// 4. Open Bookmark Method that takes bookmark index as args
+/// 5. Switch To Previous Method to jump to previous bookmark, takes no args but previousBookmarkIndex field
+/// 6. Save Bookmark Method to save bookmark, takes SVBookmark instance, index, previewTexture
+/// 7. 
+/// </summary>
 static class SVBookmarkManager 
 {
     public static event Action<int> MoveToBookmarkEvent;
-    public static event Action<int, SceneViewBookmark, Texture2D> SetBookmarkEvent;
+    public static event Action<int, SVBookmark, Texture2D> SetBookmarkEvent;
+    public static event Action ClearAllBookmarkEvent;
+
+    private const string SVBookmarkIconBlackGUID = "";
+    private const string SVBookmarkIconWhiteGUID = "";
+    private const string SVEmptyIconGUID = "";
 }
